8503526691f4774e8a0106ad59bc4dcbf12c676f
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-03-09 15:19:18 +0000

    added exercise detail page and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor='#fff3f4'>
+      <Stack gap='20px' alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h6" pb="20px" mt="5px">
+          Made by Lewis Botcherby 2023
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
